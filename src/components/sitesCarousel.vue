fec00b90534e76e59b97e95b3f76b45ef28b144c
Saymon Felipe <linnubr@gmail.com>
2024-09-24 23:29:37 -0300

    Inclusao de novos projetos

--- src/components/sitesCarousel.vue
+++ src/components/sitesCarousel.vue
@@ -6,6 +6,12 @@
                     <img src="https://rabsystems-storage.s3.sa-east-1.amazonaws.com/portal-aluno-thumb.png" dataTargetLink="solutto.com.br/segmento-franquia-educacional.aspx" v-on:click="expandImage($event)" />
                 </div>
                 <div class="swiper-slide">
+                    <img src="https://rabsystems-storage.s3.sa-east-1.amazonaws.com/sistema-solutto-thumb.png" dataTargetLink="sistema.solutto.com.br" v-on:click="expandImage($event)" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://rabsystems-storage.s3.sa-east-1.amazonaws.com/sistema-mokaly-thumb.png" dataTargetLink="app.mokaly.com" v-on:click="expandImage($event)" />
+                </div>
+                <div class="swiper-slide">
                     <img src="https://rabsystems-storage.s3.sa-east-1.amazonaws.com/portal-aluno-2-thumb.png" dataTargetLink="solutto.com.br/segmento-franquia-educacional.aspx" v-on:click="expandImage($event)" />
                 </div>
                 <div class="swiper-slide">
